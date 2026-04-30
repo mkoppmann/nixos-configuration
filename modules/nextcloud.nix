e8f9bb2b2f34b474959ae8352cee8e06db889f63
mkoppmann <dev@mkoppmann.at>
2026-04-30 22:59:25 +0200

    Nextcloud: Upgrade to Nextcloud 33

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -8,7 +8,7 @@
   services.nextcloud = {
     enable = true;
     hostName = "cloud.ncrypt.at";
-    package = pkgs.nextcloud32;
+    package = pkgs.nextcloud33;
 
     configureRedis = true;
     database.createLocally = false;
